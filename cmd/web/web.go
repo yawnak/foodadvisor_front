3eac697a75a5c0372965a014cc25b6e9e413a061
asstronom <shliakhov.danylo@gmail.com>
2023-04-13 17:50:54 +0300

    main

--- cmd/web/web.go
+++ cmd/web/web.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"log"
+
+	"github.com/yawnak/foodadvisor_front/internal/server"
+)
+
+func main() {
+	srv := server.NewServer()
+	log.Fatalln(srv.ListenAndServe("3000"))
+}
