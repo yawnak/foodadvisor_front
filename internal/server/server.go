0f017d559e1509a05933e5262d732e66e9db328f
asstronom <shliakhov.danylo@gmail.com>
2023-04-13 17:50:48 +0300

    server

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,5 +1,25 @@
 package server
 
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
 type Server struct {
 	router *chi.Mux
+}
+
+func NewServer() *Server {
+	srv := Server{}
+	router := chi.NewRouter()
+	router.Mount("/", srv.initRoutes())
+	srv.router = router
+	return &srv
+}
+
+
+
+func (srv *Server) ListenAndServe(port string) error {
+	return http.ListenAndServe(":"+port, srv.router)
 }
