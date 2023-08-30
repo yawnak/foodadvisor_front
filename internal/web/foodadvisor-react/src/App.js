03d23c680636927a9b9858aee426573fced0feae
asstronom <shliakhov.danylo@gmail.com>
2023-08-30 14:58:05 +0300

    feat: add browserrouter

--- internal/web/foodadvisor-react/src/App.js
+++ internal/web/foodadvisor-react/src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+  NavLink,
+} from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route index element={<Home />}></Route>
+      <Route path="/about" element={<About/>}></Route>
+    </Route>
+  )
+)
 
 function App() {
   return (
-    <div className="App">
-      <h1>Food Advisor</h1>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
