b407249f73bff71b86ea13956400ef7b248c69bc
asstronom <shliakhov.danylo@gmail.com>
2023-09-01 22:36:40 +0300

    fix: URL

--- internal/web/foodadvisor-react/src/Axios.js
+++ internal/web/foodadvisor-react/src/Axios.js
@@ -1,7 +1,7 @@
-import axios from axios
+import axios from 'axios'
 
 export const foodAdvisorClient = axios.create(
     {
-        baseURL: 'localhost:8080/api',
+        baseURL: 'http://localhost:8080/api',
     }
 )
