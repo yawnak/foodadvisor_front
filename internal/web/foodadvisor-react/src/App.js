0163fac3d593729b17f22e1838cc8e6b88b820b1
asstronom <shliakhov.danylo@gmail.com>
2023-09-01 18:32:47 +0300

    feat: redo routes and add /login and /signup

--- internal/web/foodadvisor-react/src/App.js
+++ internal/web/foodadvisor-react/src/App.js
@@ -9,12 +9,18 @@ import {
 import Home from './pages/Home';
 import About from './pages/About';
 import RootLayout from './layouts/RootLayout';
+import Login from './pages/Login';
+import SignUp from './pages/Signup';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
-      <Route index element={<Home />}></Route>
-      <Route path="/about" element={<About/>}></Route>
+    <Route path="/">
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/signup" element={<SignUp />}></Route>
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Route>
     </Route>
   )
 )
