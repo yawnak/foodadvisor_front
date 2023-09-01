93a64f25aae2d1e96c1c792e289daff7536039d8
asstronom <shliakhov.danylo@gmail.com>
2023-09-01 22:38:17 +0300

    feat: password text input field with icon to hide

--- internal/web/foodadvisor-react/src/pages/Login.js
+++ internal/web/foodadvisor-react/src/pages/Login.js
@@ -1,29 +1,58 @@
-import { Button } from '@mui/material';
+import { Button, FilledInput, FormControl, IconButton, InputAdornment, InputLabel, LinearProgress, Stack } from '@mui/material';
 import TextField from '@mui/material/TextField';
 import { Link } from 'react-router-dom';
+import { foodAdvisorClient } from '../Axios';
+import { VisibilityOff, Visibility } from '@mui/icons-material'
+import { useState } from 'react';
+
 
 export default function Login() {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+        setShowPassword((show) => !show);
+    };
+
+
     return (
         <div className='login'>
-            <h2>Welcome</h2>
-            <TextField
-                label="Username"
-                required
-                id="login-input-username"
-                variant='filled'
-            />
-            <TextField
-                label='Password'
-                required
-                id="login-input-password"
-                variant='filled'
-            />
-            <Button
-                variant='contained' size='large' color='secondary'>
-                Log in
-            </Button>
-            <p>Don't have an account? Sign up <Link to="/signup">here</Link></p>
-
+            <Stack component={"form"}>
+                <h2>Welcome</h2>
+                <TextField
+                    label="Username"
+                    required
+                    id="login-input-username"
+                    variant='filled'
+                />
+                <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+                    <InputLabel required htmlFor="outlined-adornment-password">Password</InputLabel>
+                    <FilledInput
+                        id="outlined-adornment-password"
+                        type={showPassword ? 'text' : 'password'}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                >
+                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                        label="Password"
+                    />
+                </FormControl>
+                <p>Don't have an account? Sign up <Link to="/signup">here</Link></p>
+                <Button
+                    variant='contained'
+                    size='large'
+                    color='secondary'
+                    onClick={handleLoginRequest}>
+                    Log in
+                </Button>
+            </Stack>
         </div>
     )
 }
